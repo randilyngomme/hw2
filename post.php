821035902d44130a5a0521139d0323487e64314e
randilyngomme <122951455+randilyngomme@users.noreply.github.com>
2023-09-17 23:09:30 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,12 +1,12 @@
 <?php
-$pageTitle="Post";
+$PageTitle = "Post";
 include "view-header.php";
 ?>
-<h1>Post</h1>
-<form method="post" action="post.result.php">
-  <input type="text" name="my-name">
-  <input type="submit" value="Post data">
-</form>
+    <h1>Post</h1>
+<form method = "post" action = "post-result.php">
+  <input type= "text" name="my-name">
+  <input type = "submit" value = "Post Data" >
+  </form>
 <?php
 include "view-footer.php";
 ?>
