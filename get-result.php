fe55d4c50924bd9b53c94cfe8e8cb32366f5b334
randilyngomme <122951455+randilyngomme@users.noreply.github.com>
2023-09-17 23:25:11 -0500

    Update get-result.php

--- get-result.php
+++ get-result.php
@@ -1,18 +1,21 @@
 <?php
-$pageTitle="Get result";
+$pageTitle = "Get Result";
 include "view-header.php";
 ?>
-<h1>Get result</h1>
+    <h1>Get Result</h1>
 <?php
-if(isset($_GET['my-name'])){
-  ?>
-  <p>The value sent is:</p>
-  <?php
+if (isset($_GET['my-name']))
+{
+?>
+<p>The value sent is: </p>
+<?php
   echo $_GET['my-name'];
-  }else{
-  ?>
-  <p>Nothing sent to the page</p>
-  <?php
-  }
+}
+else
+{
+?>
+<p>Nothing  was sent to the page</p>
+<?php 
+}
 include "view-footer.php";
 ?>
