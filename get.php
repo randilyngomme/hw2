c2202834bcb22d8fe73cfb2f01e5f12e36d0890e
randilyngomme <122951455+randilyngomme@users.noreply.github.com>
2023-09-17 23:32:28 -0500

    Update get.php

--- get.php
+++ get.php
@@ -4,7 +4,7 @@ include "view-header.php";
 ?>
     <h1>Get</h1>
 <form method = "get" action = "get-result.php">
-  <input type="text" name = 'my-name'>
+  <input type="text" name = "my-name">
   <input type ="submit" value = "Get Data">
   </form>
 <?php
